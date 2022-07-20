2b9a34b8ec707246d7d4ed5055ee8a96449833a0
immmmmmmmma <icanasd@gmail.com>
2022-07-20 18:17:58 +0900

    07company v1

---
+++ 07osc/js/script.js
@@ -0,0 +1,23 @@
+console.log("index");
+
+$(".navbar>ul>li").hover(function(){
+    $(this).addClass("on");
+},function(){
+    $(this).removeClass("on");
+});
+$(".fncUtil>li").hover(function(){
+    $(this).addClass("on");
+},function(){
+    $(this).removeClass("on");
+});
+
+$(".toggle").click(function(){
+    $(".navbarClone").css("left",0);
+    $(".overlay").show();
+})
+$(".overlay").click(function(){
+    $(".navbarClone").css("left","-100%");
+    $(".overlay").hide();
+});
+
+$(".navbar>ul").clone().append()
