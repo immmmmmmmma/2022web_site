9156aa7c8de0f95c61ed715e08e2767afcee736a
immmmmmmmma <icanasd@gmail.com>
2022-07-22 18:18:39 +0900

    07osc v2

--- 07osc/js/script.js
+++ 07osc/js/script.js
@@ -11,6 +11,32 @@ $(".fncUtil>li").hover(function(){
     $(this).removeClass("on");
 });
 
+$(".navbarWrap").hover(function(){
+    $(".black").show();
+    $(".white").hide();
+},function(){
+    $(".white").show();
+    $(".black").hide();
+})
+
+$(".navbarWrap").mouseenter(function(){
+    $(".navbar>ul>li>a").css("color","#333");
+    $(".fncUtil>li>a").css("color","#333");
+})
+$(".navbarWrap").mouseleave(function(){
+    $(".navbar>ul>li>a").css("color","#fff");
+    $(".fncUtil>li>a").css("color","#fff");
+})
+$(".navbarWrap").mouseenter(function(){
+    $("#header").css("height","100px")
+    $("#header").css("background-color","#fff")
+})
+$(".navbarWrap").mouseleave(function(){
+    $("#header").css("height","initial")
+    $("#header").css("background-color","initial")
+})
+
+
 $(".toggle").click(function(){
     $(".navbarClone").css("left",0);
     $(".overlay").show();
