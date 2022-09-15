2940272d0e9e187888bc3dc60a03756c65f5b17c
immmmmmmmma <icanasd@gmail.com>
2022-09-15 18:17:49 +0900

    20220915_full v1

--- 09fullpage/js/script.js
+++ 09fullpage/js/script.js
@@ -1,5 +1,4 @@
 $("#fullpage").fullpage({
-    menu:".navbar",
     anchors:["1st","2nd","3rd","4th","5th"],
     navigation:true,
     navigationPosition:"fp-right",
@@ -8,29 +7,45 @@ $("#fullpage").fullpage({
     afterLoad:function(anchorslink,index){
         console.log(anchorslink,index);
         if(index == 1){
-            $(".page1 .txtWrap").addClass("move")
+            $(".page1 .wrap").addClass("move")
+            $("body").css("background","rgb(0,68,56)")
+            $("h2").css("color","#fff")
+            $("p").css("color","#577e77").css("font-size","72px").css("line-height","100px").css("letter-spacing","18px")
         }else{
-            $(".page1 .txtWrap").removeClass("move")
+            $(".page1 .wrap").removeClass("move")
         }
         if(index == 2){
-            $(".page2 .txtWrap").addClass("move")
+            $(".page2 .wrap").addClass("move")
+            $("body").css("background","rgb(248,232,193)")
+            $("h2").css("color","#bca261")
+            $("p").css("color","#333").css("font-size","36px").css("line-height","54px").css("letter-spacing","initial")
+            
         }else{
-            $(".page2 .txtWrap").removeClass("move")
+            $(".page2 .wrap").removeClass("move")
         }
         if(index == 3){
             $(".page3 .wrap").addClass("move")
+            $("body").css("background","rgb(149,150,106)")
+            $("h2").css("color","#fff")
+            $("p").css("color","#fff")
         }else{
             $(".page3 .wrap").removeClass("move")
         }
         if(index == 4){
-            $(".page4 .txtWrap").addClass("move")
+            $(".page4 .wrap").addClass("move")
+            $("body").css("background","rgb(148,118,94)")
+            // $("h2").css("color","#fff")
+            // $("p").css("color","#fff")
         }else{
-            $(".page4 .txtWrap").removeClass("move")
+            $(".page4 .wrap").removeClass("move")
         }
         if(index == 5){
-            $(".page5 .txtWrap").addClass("move")
+            $(".page5 .wrap").addClass("move")
+            $("body").css("background","rgb(27,40,76)")
+            // $("h2").css("color","#fff")
+            // $("p").css("color","#fff")
         }else{
-            $(".page5 .txtWrap").removeClass("move")
+            $(".page5 .wrap").removeClass("move")
         }
     }
 })
